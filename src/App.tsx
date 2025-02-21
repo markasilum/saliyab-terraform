f4d5b4095b8a31029b2482298fddcaed5df058b2
markasilum <markasilum13@gmail.com>
2025-02-21 15:20:46 +0800

    new

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
           className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
           alt="logo"
         />
-        <p className="text-[#7B42BC] font-bold">Saliyab Terraform 🎉</p>
+        <p className="text-[#7B42BC] font-bold">THIS IS MY SITEEEE 🎉</p>
       </header>
     </div>
   );
